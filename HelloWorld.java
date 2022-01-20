c0cc749c35a95ffaa106246284bd244ddd115c62
Caillan Lovell <clovell@softwareinstitute.com>
2022-01-20 16:54:04 +0000

    New change

--- HelloWorld.java
+++ HelloWorld.java
@@ -15,6 +15,8 @@ public class HelloWorld {
         int Walllength1 = myObj.nextInt();
         int Walllength2 = myObj.nextInt();
         int Wallheight = myObj.nextInt();
+        System.out.println("Enter budget:");
+        int Budget = myObj.nextInt();
         int Surface_Area = 2 * (Walllength1+Walllength2) * Wallheight;
         double Numberofcans = (double) (Surface_Area/PaintCoveragePerLitre)/Litrespercan;
         int Numberofcanswhole = (int) Math.ceil(Numberofcans);
@@ -35,36 +37,32 @@ public class HelloWorld {
         int Cost1 = Costpercan1*Numberofcanswhole;
         int Cost2 = Costpercan2*Numberofcanswhole;
         int MinCost = Math.min(Cost1,Cost2);
-        if (MinCost == Cost1 && Cost2 != Cost1){
-            System.out.println("Length of walls are "+ Walllength1 +" and "+ Walllength2+".");
-            System.out.println("Height of wall is "+ Wallheight+".");
-            System.out.println("The cost to decorate the house with " + PaintName1 + " is £" + Cost1 +".");
-            System.out.println("The cost to decorate the house with " + PaintName2 + " is £" + Cost2+ ".");
+
+        System.out.println("Length of walls are "+ Walllength1 +" and "+ Walllength2+".");
+        System.out.println("Height of wall is "+ Wallheight+".");
+        System.out.println("The cost to decorate the house with " + PaintName1 + " is £" + Cost1 +".");
+        System.out.println("The cost to decorate the house with " + PaintName2 + " is £" + Cost2+ ".");
+
+        if ((MinCost == Cost1) && (Cost2 != Cost1)){
             System.out.println("The cheapest to paint the house is £" + MinCost + " using " + PaintName1 +". With a labour cost of £" +
                     Labourcost +" the total price is £" + (MinCost+ Labourcost)+".");
-            System.out.println("The job originally would have taken "+ intTimeindaysforjob +
-                    " day(s), however it rained for " + Rainydays + " day(s), so the job took " +realTimeindaysforjob +" days.");
         }
-        if (MinCost == Cost2 && Cost2 != Cost1){
-            System.out.println("The length of the walls are "+ Walllength1 +"m and "+ Walllength2+"m.");
-            System.out.println("The height of the walls is "+ Wallheight+"m.");
-            System.out.println("The cost to decorate the house with " + PaintName1 + " is £" + Cost1 +".");
-            System.out.println("The cost to decorate the house with " + PaintName2 + " is £" + Cost2+ ".");
+        else if ((MinCost == Cost2) && (Cost2 != Cost1)){
             System.out.println("The cheapest to paint the house is £" + MinCost + " using " + PaintName2 +". With a labour cost of £" +
                     Labourcost +" the total price is £" + (MinCost+ Labourcost)+".");
-            System.out.println("The job originally would have taken "+ intTimeindaysforjob +
-                    " day(s), however it rained for " + Rainydays + " day(s), so the job took " +realTimeindaysforjob +" days.");
+
         }
-        else{
-            System.out.println("The length of the walls are "+ Walllength1 +"m and "+ Walllength2+"m.");
-            System.out.println("The height of the walls is "+ Wallheight+"m.");
-            System.out.println("The cost to decorate the house with " + PaintName1 + " is £" + Cost1 +".");
-            System.out.println("The cost to decorate the house with " + PaintName2 + " is £" + Cost2+ ".");
+        else if (Cost1 == Cost2){
             System.out.println("Both paints cost the same amount, £" + MinCost + ", so either can be used. With a labour cost of £" +
                     Labourcost +" the total price is £" + (MinCost+ Labourcost) + ".");
-            System.out.println("The job originally would have taken "+ intTimeindaysforjob +
-                    " day(s), however it rained for " + Rainydays + " day(s), so the job took " +realTimeindaysforjob +" days.");
         }
+        else {
+            System.out.println("Error");
+        }
+        System.out.println("The job originally would have taken "+ intTimeindaysforjob +
+                " day(s), however it rained for " + Rainydays + " day(s), so the job took " +realTimeindaysforjob +" days.");
+        String result = ((MinCost+ Labourcost) >= Budget) ? "This job is over budget." : "This job is under budget.";
+        System.out.println(result);
 
     }
 }
