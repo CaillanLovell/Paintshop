988e34de5181ae4731af3cb86c839201f7883f54
Caillan Lovell <clovell@softwareinstitute.com>
2022-01-21 02:24:48 +0000

    More changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,10 +7,10 @@ public class HelloWorld {
         String PaintName1 = "Classy";
         String PaintName2 = "Fancy";
         Scanner myObj = new Scanner(System.in);
-        int PaintCoveragePerLitre = 8;
+        int PaintCoveragePerLitre = 12;
         int Litrespercan = 5;
-        int Costpercan1 = (int) (Math.ceil(Math.random()*30)+20);
-        int Costpercan2 = (int) (Math.ceil(Math.random()*30)+20);
+        int Costpercan1 = (int) (Math.ceil(Math.random()*20)+20);
+        int Costpercan2 = (int) (Math.ceil(Math.random()*20)+20);
         System.out.println("Enter house length, width and height:");
         int Walllength1 = myObj.nextInt();
         int Walllength2 = myObj.nextInt();
@@ -38,7 +38,7 @@ public class HelloWorld {
         int Cost2 = Costpercan2*Numberofcanswhole;
         int MinCost = Math.min(Cost1,Cost2);
 
-        System.out.println("Length of walls are "+ Walllength1 +" and "+ Walllength2+".");
+        System.out.println("Length of walls are "+ Walllength1 +" and "+ Walllength2 +".");
         System.out.println("Height of wall is "+ Wallheight+".");
         System.out.println("The cost to decorate the house with " + PaintName1 + " is £" + Cost1 +".");
         System.out.println("The cost to decorate the house with " + PaintName2 + " is £" + Cost2+ ".");
@@ -47,22 +47,26 @@ public class HelloWorld {
             System.out.println("The cheapest to paint the house is £" + MinCost + " using " + PaintName1 +". With a labour cost of £" +
                     Labourcost +" the total price is £" + (MinCost+ Labourcost)+".");
         }
-        else if ((MinCost == Cost2) && (Cost2 != Cost1)){
+        else if (Cost2 != Cost1){
             System.out.println("The cheapest to paint the house is £" + MinCost + " using " + PaintName2 +". With a labour cost of £" +
                     Labourcost +" the total price is £" + (MinCost+ Labourcost)+".");
 
         }
-        else if (Cost1 == Cost2){
+        else {
             System.out.println("Both paints cost the same amount, £" + MinCost + ", so either can be used. With a labour cost of £" +
                     Labourcost +" the total price is £" + (MinCost+ Labourcost) + ".");
-        }
-        else {
-            System.out.println("Error");
         }
         System.out.println("The job originally would have taken "+ intTimeindaysforjob +
                 " day(s), however it rained for " + Rainydays + " day(s), so the job took " +realTimeindaysforjob +" days.");
         String result = ((MinCost+ Labourcost) >= Budget) ? "This job is over budget." : "This job is under budget.";
         System.out.println(result);
+
+        switch (Numberofcanswhole) {
+            case 1 -> System.out.println("Only one tin needed");
+            case 2, 3, 4, 5 -> System.out.println("A few tins needed");
+            case 6, 7, 8, 9, 10, 11, 12 -> System.out.println("Many tins needed");
+            default -> System.out.println("Something has gone wrong");
+        }
 
     }
 }
